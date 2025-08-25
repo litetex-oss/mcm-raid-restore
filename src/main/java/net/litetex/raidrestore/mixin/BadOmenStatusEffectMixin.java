54e648a3a36e82e0bec4619deee480dd65f073f9
litetex <40789489+litetex@users.noreply.github.com>
2025-08-25 21:31:15 +0200

    Cleanup code

--- src/main/java/net/litetex/raidrestore/mixin/BadOmenStatusEffectMixin.java
+++ src/main/java/net/litetex/raidrestore/mixin/BadOmenStatusEffectMixin.java
@@ -22,15 +22,17 @@ public abstract class BadOmenStatusEffectMixin
 		final int amplifier,
 		final CallbackInfoReturnable<Boolean> cir)
 	{
-		if(entity instanceof final ServerPlayerEntity player && !player.isSpectator())
+		if(entity instanceof final ServerPlayerEntity player
+			&& !player.isSpectator()
+			&& world.getDifficulty() != Difficulty.PEACEFUL)
 		{
 			final BlockPos playerBlockPos = player.getBlockPos();
-			if(world.getDifficulty() != Difficulty.PEACEFUL
-				&& world.isNearOccupiedPointOfInterest(playerBlockPos))
+			if(world.isNearOccupiedPointOfInterest(playerBlockPos))
 			{
 				world.getRaidManager().startRaid(player, playerBlockPos);
 			}
-			cir.setReturnValue(true);
+			// Returning false here would remove the effect, however this is handled by Raid#start
+			// (to determine the raid level)
 		}
 		cir.setReturnValue(true);
 	}
